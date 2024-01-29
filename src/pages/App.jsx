74df9e4b4a0c1b8a35f41f549741b026e1770836
JacobsM14 <marcel.esplu@gmail.com>
2024-01-29 13:27:07 +0100

    fighting with tailwind

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import logo from './../assets/logo.png'
+import cv from "/SVG/links/cv.svg"
+import github from "/SVG/links/github.svg"
+import linkedin from "/SVG/links/linkedin.svg"
+import medium from "/SVG/links/medium.svg"
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */ }
@@ -11,18 +15,32 @@ function App() {
   return (
     <>
       <div className="">
-        <header className='grid grid-cols-17 mx-auto flex items-center'>
-          <nav className='col-span-17'>
-            <ul className='grid grid-cols-11 gap-3 justify-center'>
-              <li className='col-span-2 text-center'><a href="#">Home</a></li>
-              <li className='col-span-2 text-center'><a href="#">Experiences</a></li>
-              <li className='col-span-2 text-center'><a href="#">Technologies</a></li>
-              <li className='col-span-2 text-center'><a href="#">Projects</a></li>
-            </ul>
+        <header className='padding-starter grid grid-cols-17 gap-3 mx-auto items-center justify-items-center'>
+          <nav className='grid col-span-17 grid-cols-11 gap-2'>
+            <a className='col-span-2 text-center' href="">Home</a>
+            <a className='col-span-3 text-center' href="">Experiences</a>
+            <a className='col-span-3 text-center' href="">Technologies</a>
+            <a className='col-span-3 text-center' href="">Projects</a>
           </nav>
+          <div className='info-header-content grid grid-cols-17 col-span-13 col-start-2'>
+            <div className="title-content grid col-span-7 grid-cols-7 col-start-3">
+              <h1 className='col-span-7'>Hi,I’m Marcel.</h1>
+              <p className='col-span-7'>I build websites.</p>
+            </div>
+            <div className='images-and-links grid col-span-6 grid-cols-6 col-start-12'>
+              <img src={logo} alt="logo" className='col-span-6' />
+              <div className="url-svg">
+                <a href=""><img src={cv} alt="cv" /></a>
+                <a href=""><img src={github} alt="github" /></a>
+                <a href=""><img src={linkedin} alt="linkedin" /></a>
+                <a href=""><img src={medium} alt="medium" /></a>
+              </div>
+            </div>
+          </div>
         </header>
       </div>
     </>
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,12 @@ module.exports = {
     },
     extend: {
       gridColumn: {
+        'span-7': 'span 7 / span 7',
+        'span-8': 'span 8 / span 8',
+        'span-9': 'span 9 / span 9',
+        'span-10': 'span 10 / span 10',
+        'span-11': 'span 11 / span 11',
+        'span-12': 'span 12 / span 12',
         'span-13': 'span 13 / span 13',
         'span-14': 'span 14 / span 14',
         'span-15': 'span 15 / span 15',

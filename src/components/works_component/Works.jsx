4c18c6b5ffae8397a276e9322cf66dc53d97f66f
JacobsM14 <marcel.esplu@gmail.com>
2024-02-28 13:24:31 +0100

    DESKTOP VERSION COMPLETE

--- src/components/works_component/Works.jsx
+++ src/components/works_component/Works.jsx
@@ -1,35 +1,48 @@
-import React from 'react'
+import React, { useId, useState } from 'react'
 
+export const Works = ({ img, work, empresa, start, working, final, active, setActive }) => {
 
+  // CHANGE VISIBILITY OF WORKS FT. YasaiPajas
+  const [index, setWorkData] = useState({
 
-export const Works = ({ work, empresa, start, working }) => {
+  })
+  const id = useId();
+
+  const handleClick = () => {
+    setActive(id);
+  }
+
+  const unsetActive = () => {
+    setActive(null);
+  }
+
   return (
     <>
       <div className='work-display'>
         <div className="cont-data-work">
           <div className='flex justify-between items-center flex-wrap'>
             <div className="img-title-works flex flex-start items-center">
-              <div className="imagen-prueba"></div>
+              <img className="imagen-prueba" src={img} alt="" />
               <h5>{work}</h5>
             </div>
             <div className='icons-arrow flex'>
-              <button><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 14l-5-5m0 0l-5 5" /></svg></button>
-              <button className='button-work-none'><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10l5 5m0 0l5-5" /></svg></button>
+              <button style={{ display: `${active === id ? 'none' : 'block'}`}} onClick={handleClick}><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 14l-5-5m0 0l-5 5" /></svg></button>
+              <button style={{ display: `${active === id ? 'block' : 'none'}` }} onClick={unsetActive}><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10l5 5m0 0l5-5" /></svg></button>
             </div>
           </div>
-          <div className="lista">
+          <div className="lista" style={{ display: `${active === id ? 'block' : 'none'}`, transition: "1s"}}>
             <ul>
               <li><span>Worked on: </span> {empresa} </li>
               <li><span>Started on: </span>{start}</li>
-              <li><span>{working == "true" ? "Currently Working" : "Finished"}</span></li>
+              {working == "true" ? <li><span>Currently Working</span></li> : <li><span>Finished on:</span>{final}</li>}
             </ul>
           </div>
         </div>
-          <div className='logos flex flex-end '>
-            <img src="Images/tech-logos/node.png" alt="" />
-            <img src="Images/tech-logos/react.png" alt="" />
+        <div className='logos flex flex-end' style={{ display: `${active === id ? 'flex' : 'none'}`, transition: "1s"}}>
+          <img src="Images/tech-logos/node.png" alt="" />
+          <img src="Images/tech-logos/react.png" alt="" />
 
-          </div>
+        </div>
       </div>
     </>
   )
